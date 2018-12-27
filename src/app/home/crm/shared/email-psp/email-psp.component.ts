ea3a3cab5c69584019f5e2e1fb4600b5a644a2ed
Alejandro Quadri <ale@quadri.com.ar>
2018-12-27 13:48:08 -0300

    terminado componente de envio de mails

--- src/app/home/crm/shared/email-psp/email-psp.component.ts
+++ src/app/home/crm/shared/email-psp/email-psp.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { CrmService } from '../crm.service';
+import { LoggerService } from '../../../../auth/shared/logger.service';
+import * as moment from 'moment';
+
+import { map } from 'rxjs/operators';
+import { NavParams, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-email-psp',
@@ -7,9 +14,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailPspComponent implements OnInit {
 
-  constructor() { }
+  emailForm: FormGroup;
+  psp: any;
+  op: any;
+  currentEmail: string;
+  currentSalesMan: string;
+
+  constructor(
+    // public navCtrl: NavController,
+    public modalCtrl: ModalController,
+    public navParams: NavParams,
+    private fb: FormBuilder,
+    private crmData: CrmService,
+    private authData: LoggerService
+  ) {
+    this.buildForm();
+    this.currentEmail = this.authData.current.email;
+    this.currentSalesMan = this.crmData.currentSalesRep;
+    console.log(this.currentEmail);
+   }
 
   ngOnInit() {
+    console.log(this.navParams.data.psp, this.navParams.data.op);
+    this.psp = this.navParams.data.psp;
+    this.op = this.navParams.data.op;
+  }
+
+  buildForm() {
+    this.emailForm = this.fb.group({
+      to: ['', [Validators.required, Validators.email]],
+      cc: [''],
+      obs: ['']
+    });
+  }
+
+  onSubmit() {
+    const today = moment().format('YYYY-MM-DD');
+
+    const emailForm = this.emailForm.value;
+    emailForm['date'] = this.psp.date;
+    emailForm['number'] = this.psp.num;
+    emailForm['razSoc'] = this.psp.razSoc;
+    emailForm['salesRep'] = this.psp.salesRep;
+    emailForm['total'] = this.psp.total;
+    emailForm['items'] = this.psp.items;
+    emailForm['iva'] = this.psp.ivaTotal;
+    emailForm['iibb'] = this.psp.iibb;
+    emailForm['final'] = this.psp.totalFinal;
+    emailForm['currentEmail'] = this.currentEmail;
+    console.log(emailForm);
+
+    let activityForm = {
+      time: today,
+      action: 'Envio de psp',
+      desc: `Envio presupuesto ${this.psp.num} a ${emailForm.to}`,
+      complete: true,
+      salesRep: this.currentSalesMan
+    };
+
+    if (this.op) {
+      activityForm = this.op;
+      activityForm['time'] = today;
+      activityForm['action'] = 'Envio de psp';
+      activityForm['desc'] = `Envio presupuesto ${this.psp.num} a ${emailForm.to}`;
+      activityForm['complete']  = true;
+    }
+
+    this.crmData.postPsp(emailForm)
+    .pipe(
+      map( res => res.json())
+    )
+    .toPromise()
+    .then( res => {
+      console.log('mail enviado');
+      this.crmData.newAgendaNote(activityForm);
+    })
+    .then( ret =>  {
+      console.log('activity log cargado', ret);
+      this.modalCtrl.dismiss('dismiss');
+    })
+    .catch( reason => {
+      console.log('error en envio' , reason);
+    });
   }
 
 }
